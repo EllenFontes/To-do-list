86acb04fe84d530bc8c20cd58aad47b61e01e37b
EllenFontes <fontes.ellen21@gmail.com>
2025-05-16 16:13:19 -0300

    fix: creation of handleMethodArgumentNotValid for standarlizing error messages related to required field

--- src/main/java/com/todoapp/todolist/infra/RestExceptionHandler.java
+++ src/main/java/com/todoapp/todolist/infra/RestExceptionHandler.java
@@ -1,19 +1,53 @@
 package com.todoapp.todolist.infra;
 
 import com.todoapp.todolist.exception.TaskNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.StringJoiner;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    //Tratamento de erro: validações dos campos enviados
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        StringJoiner joiner = new StringJoiner("; ");
+
+        for (var error : exception.getBindingResult().getAllErrors()) {
+            String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            joiner.add(field + ": " + message);
+        }
+
+        ApiError apiError = ApiError.builder()
+                .timestamp(LocalDateTime.now())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .status(HttpStatus.BAD_REQUEST.name())
+                .message("Validation has failed for one or more fields")
+                .error(joiner.toString())
+                .build();
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+
+    //Tratamento de erro: Task não encontrada
 
     @ExceptionHandler(TaskNotFoundException.class)
     private ResponseEntity<ApiError> taskNotFoundHandler(TaskNotFoundException exception){
